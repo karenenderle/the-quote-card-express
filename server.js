633317e5cacc469cc4cf15eebc3a61f51549c530
Karen Enderle <kee.enderle@gmail.com>
2025-10-17 21:19:37 -0400

    Install nodemon and basic revisions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const port = 8080;
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
