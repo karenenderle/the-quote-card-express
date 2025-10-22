67d1f3783ab65420381b771fabe1f6c7b52f00ea
Karen Enderle <kee.enderle@gmail.com>
2025-10-21 22:50:12 -0400

    Implement Unsplash image fetch

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,26 @@
-"use strict"
-
-const elements = {
+/* const elements = {
     quote: document.getElementById("quote"),
     author: document.getElementById("author"),
-};
+}; */
 
-const quotes = [
+async function getRandomImage() {
+    const client_id = "YOUR_ACCESS_KEY";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url("${receivedPhotoUrl}")`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage();
+
+/* const quotes = [
     {
         quote: "All hands! Abandon ship!",
         author: "Captain Picard",
@@ -35,4 +50,4 @@ function loopThroughQuotes() {
     }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000); */
